0e5ad3cfb50e02af6389fa68f57351688116ef48
Favour Emeka <spellzcfi@gmail.com>
2024-01-06 18:56:53 +0100

    update

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function toggleDarkMode() {
+    let bodyDarkMode = document.body;
+    bodyDarkMode.style.backgroundColor = "rgba(23, 23, 28, 1)";
+
+    let containerDarkMode = document.getElementById ("projectContainer");
+    containerDarkMode.style.backgroundColor ="rgba(23, 23, 28, 1)";
+
+    let resultDarkMode = document.getElementById ("result");
+    resultDarkMode.style.color = "rgba(255, 255, 255, 1)";
+
+    
+}
